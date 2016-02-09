b4fa90ca399b331613288251bab6fb522f8916b1
Robert <robert@engagio.com>
2016-02-08 16:07:01 -0800

    changes server to allow categories

--- app/server/api/api.js
+++ app/server/api/api.js
@@ -5,12 +5,15 @@ const client = Craigslist({
 
 module.exports = (app) => {
   app.get('/craigslist/search', (req, res) => {
+    const search = req.query.search;
+    const category = req.query.cat;
     /*
       maxAsk (number),
       minAsk (number),
       category (string),
     */
     const options = {
+      category
     };
 
     client.search(options, req.query.search, (err, listings) => {
