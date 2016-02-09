76574e39746e09bc87ea4e225114a9197a3cfcf7
Robert <robert@engagio.com>
2016-02-08 16:02:51 -0800

    adds promise to dao

--- app/web/app/js/dao/app_dao.js
+++ app/web/app/js/dao/app_dao.js
@@ -6,14 +6,23 @@ import Promise from 'bluebird';
 module.exports = {
   searchCraigslist: function() {
     let timeout;
-    return function (query, cat = null) => {
-      let queryObj = {
-        search: query,
-        cat: cat
-      };
-      clearTimeout(timeout);
-      timeout = setTimeout(
-        ()=>{return AjaxUtils.get(`/craigslist/search?${querystring.stringify(queryObj)}`);}, 250)
+    return (query, cat = null) => {
+      return new Promise((resolve, reject) => {
+        let queryObj = {
+          search: query,
+          cat: cat
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(()=>{
+          AjaxUtils.get(`/craigslist/search?${querystring.stringify(queryObj)}`)
+            .then((response) => {
+              resolve(reponse);
+            })
+            .catch((err) => {
+              reject(err);
+            });
+        }, 250)
+      });
     }
   }()
 }
